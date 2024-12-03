43373c13ee96779ec8784ba9cb93aca6f7904d1c
Martin Stefcek <gcifko@gmail.com>
2024-12-03 11:35:48 +0400

    fix clippy

--- atoma-proxy/src/server/handlers/chat_completions.rs
+++ atoma-proxy/src/server/handlers/chat_completions.rs
@@ -3,7 +3,7 @@ use std::time::{Duration, Instant};
 use crate::server::{http_server::ProxyState, streamer::Streamer};
 use atoma_state::types::AtomaAtomaStateManagerEvent;
 use axum::body::Body;
-use axum::http::{HeaderName, StatusCode};
+use axum::http::StatusCode;
 use axum::response::{IntoResponse, Response, Sse};
 use axum::{extract::State, http::HeaderMap, Json};
 use serde_json::Value;
